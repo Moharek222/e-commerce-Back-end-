c11a4a82300f0e74d748db381790b979420a2e13
mohamedMoharek <almharyqm107@gmail.com>
2026-06-17 19:18:57 +0300

    fix add category logic

--- src/category/category-controllers/update-category.ts
+++ src/category/category-controllers/update-category.ts
@@ -4,8 +4,8 @@ import { Category } from "../category-model";
 
 
 interface IRequest {
-    name: string;
-    description: string;
+    name?: string;
+    description?: string;
     isActive?: boolean;
 }
 interface IResponse {
@@ -21,21 +21,36 @@ export const updateCategory: RequestHandler<{ id: string }, IResponse, IRequest>
                 message: "Invalid category id format"
             });
         }
-        const category = await Category.findByIdAndUpdate(
-            req.params.id, req.body,
-            { new: true }
-        )
-            .lean();
-        if (!category) {
+        let { name, description, isActive } = req.body;
+        const updateData: any = {};
+        if (name) updateData.name = name;
+        if (description) updateData.description = description;
+        if (isActive !== undefined) updateData.isActive = isActive;
+
+        const updatedCategory = await Category.findByIdAndUpdate(
+            req.params.id,
+            { $set: updateData }, 
+            { 
+                new: true,
+                runValidators: true
+            }
+        ).exec();
+        if (!updatedCategory) {
             return res.status(404).json({
                 message: "category not found"
             });
         }
+        
         return res.status(200).json({
             message: "category updated successfully",
-            data: category
+            data: updatedCategory
         })
     } catch (error) {
+        if ((error as any).code === 11000) {
+            return res.status(400).json({ 
+                message: "Category name already exists" 
+            });
+        }
         console.error(error);
         return res.status(500).json({ message: "Internal server error" });
     }
