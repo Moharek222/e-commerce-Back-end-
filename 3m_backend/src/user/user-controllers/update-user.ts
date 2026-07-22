07eeeb2aaed768a732025e2930d6f05be7ddfd02
mohamedMoharek <almharyqm107@gmail.com>
2026-07-22 23:44:42 +0300

    update-user-file

--- 3m_backend/src/user/user-controllers/update-user.ts
+++ 3m_backend/src/user/user-controllers/update-user.ts
@@ -1,12 +1,25 @@
 import { RequestHandler } from "express";
 import { User } from "../user-model";
+
 import { body } from "express-validator";
 
-export const completeUserInfoValidator = [
+export const updateUserValidator = [
+    body("name")
+        .optional()
+        .isString().withMessage("Name must be a text")
+        .isLength({ min: 3, max: 50 }).withMessage("Name must be between 3 and 50 characters")
+        .trim(),
+
+    body("email")
+        .optional()
+        .isEmail().withMessage("Please enter a valid email address")
+        .normalizeEmail(), 
+
     body("phone")
-        .optional() 
+        .optional()
         .isString().withMessage("Phone must be a string")
         .isMobilePhone("ar-EG").withMessage("Please enter a valid Egyptian phone number"), 
+
 
     body("address")
         .optional()
@@ -14,22 +27,23 @@ export const completeUserInfoValidator = [
 
     body("address.street")
         .if(body("address").exists())
-        .notEmpty().withMessage("Street is required when adding an address")
+        .notEmpty().withMessage("Street is required when updating address")
         .isString().withMessage("Street must be a text")
         .trim(),
 
     body("address.city")
         .if(body("address").exists())
-        .notEmpty().withMessage("City is required when adding an address")
+        .notEmpty().withMessage("City is required when updating address")
         .isString().withMessage("City must be a text")
         .trim(),
 
     body("address.country")
         .if(body("address").exists())
-        .notEmpty().withMessage("Country is required when adding an address")
+        .notEmpty().withMessage("Country is required when updating address")
         .isString().withMessage("Country must be a text")
         .trim(),
 ];
+
 interface IAddress {
     street: string;
     city: string;
@@ -37,34 +51,47 @@ interface IAddress {
 }
 
 interface IRequest {
+    id: string;
+    name?: string;
+    email?: string;
     address?: IAddress;
     phone?: string;
 }
-
 interface IResponse {
     message: string;
     data?: any;
 }
 
-export const completeUserInfo:RequestHandler<{}, IResponse, IRequest>=async (req, res) => {
+export const updateUser:RequestHandler<{}, IResponse, IRequest> = async (req, res) => {
     try{
-        const id = req.user!.id;
+        const id=req.user!.id;
         const updateObj:Partial<IRequest>={}
-        const {address,phone}=req.body;
+        const {name,email,address,phone}=req.body;
+        if(name){
+            updateObj.name=name;
+        }
+        if(email){
+            updateObj.email=email;
+        }
         if(address){
+            if(!address.street || !address.city || !address.country){
+                return res.status(400).json({
+                    message:"Address details is required"
+                });
+            }
             updateObj.address=address;
         }
         if(phone){
             updateObj.phone=phone;
         }
-        const user=await User.findByIdAndUpdate(id,{$set:updateObj},{new:true});
+        const user=await User.findByIdAndUpdate(id,{$set:updateObj},{new:true,runValidators:true});
         if(!user){
             return res.status(404).json({
                 message:"User not found"
             });
         }
         return res.status(200).json({
-            message:"User info updated successfully",
+            message:"User updated successfully",
             data:user
         });
     }catch(error){
--- 3m_backend/src/user/user-router.ts
+++ 3m_backend/src/user/user-router.ts
@@ -1,45 +1,55 @@
 import { Router } from "express";
 import { getAllUsers } from "./user-controllers/get-all-users";
-import { createUserValidator, updateUserNameValidation } from "./user-validator";
+import { createUserValidator } from "./user-validator";
 import { handleValidationErrors } from "../middlewares/handleValidationErrors";
 import { addUser } from "./user-controllers/add-user";
 import { getUserById } from "./user-controllers/get-user-by-id";
 import { deleteUserById } from "./user-controllers/delete-user-by-id";
-import { updateUserNameById } from "./user-controllers/update-user-name-by-id";
 import { upload } from "../middlewares/upload.middleware";
 import { parseAddressBox } from "../middlewares/parse-address-box";
 import { isAuthenticated } from "../middlewares/isAuthenticated.middleware";
 import { completeUserInfo, completeUserInfoValidator } from "./user-controllers/complete-user-info";
 import { isAuthorized } from "../middlewares/isAuthorized.middleware";
 import { Role } from "./user-model";
+import { updateUser, updateUserValidator } from "./user-controllers/update-user";
 
 const router = Router();
 
-router.get('/',getAllUsers);
+router.get('/',
+    isAuthenticated,
+    isAuthorized(Role.Admin),
+    getAllUsers);
+
 router.get('/:id',
     isAuthenticated,
     isAuthorized(Role.Admin),
     getUserById);
 
 router.post('/add',
+    isAuthenticated,
+    isAuthorized(Role.Admin),
     upload.single('profileImage'),
     parseAddressBox,
     createUserValidator,
     handleValidationErrors,
     addUser
 );
-router.post('/address',
+router.post('/complete-info',
     isAuthenticated,
     completeUserInfoValidator,
     handleValidationErrors,
     completeUserInfo
 );
 
-router.put('/:id',
-    updateUserNameValidation,
+router.put('/update',
+    isAuthenticated,
+    updateUserValidator,
     handleValidationErrors,
-    updateUserNameById
+    updateUser
 );
-router.delete('/:id',deleteUserById);
+router.delete('/:id',
+    isAuthenticated,
+    isAuthorized(Role.Admin),
+    deleteUserById);
 
 export default router;
